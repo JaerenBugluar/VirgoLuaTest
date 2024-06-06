7a1b0731133b5371e0ba649866e5bf8c1d9a2deb
JaerenBugluar <159459193+JaerenBugluar@users.noreply.github.com>
2024-06-06 02:49:26 -0700

    VirgoLuaTest.lua

--- VirgoLuaTest.lua
+++ VirgoLuaTest.lua
@@ -5,7 +5,7 @@ local Tab1 = Win:Tab("General",14477284625)
 
 local Page1 = Tab1:CraftPage(1)
 Page1:Seperator("JOB ID")
-local Page2 = Tab1:CraftPage(1)
+local Page2 = Tab1:CraftPage(2)
 Page2:Seperator("JOIN JOB ID")
 
 Page1:Textbox('Job ID',nil, function(jobid)
